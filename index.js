714ec01021df2bccc27f7c6480d42632ec743713
Phil Ecker <hello@philecker.com>
2023-06-01 18:47:25 -0500

    Created first NPM package

---
+++ index.js
@@ -0,0 +1,6 @@
+//index.js
+function copyJSON(jsonObj) {
+  return JSON.parse(JSON.stringify(jsonObj));
+}
+
+module.exports = copyJSON;``
---
+++ test/script.js
@@ -0,0 +1,22 @@
+const copyJSON = require('copy-json');
+
+var jsonObj = {
+  "name": "John",
+  "age": 30,
+  "cars": [
+    {
+      "name": "Ford",
+      "models": ["Fiesta", "Focus", "Mustang"]
+    },
+    {
+      "name": "BMW",
+      "models": ["320", "X3", "X5"]
+    },
+    {
+      "name": "Fiat",
+      "models": ["500", "Panda"]
+    }
+  ]
+}
+
+console.log(copyJSON(jsonObj));
